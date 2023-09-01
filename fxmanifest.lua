295b48b20ae252141095e5dafe1b35650fcfc91a
Felipe Lima <70120728+Felipeex@users.noreply.github.com>
2023-08-31 22:38:52 -0300

    deleta tablet config

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,15 +2,6 @@ fx_version "adamant"
 game "gta5"
 lua54 "yes"
 
-ui_page "http://authetication.blacknetwork.com.br:4001/bspolice/index.html"
-
-shared_scripts {
-	"config.lua",
-	"@vrp/lib/utils.lua",
-	"@vrp/lib/vehicles.lua",
-	"lib/api.lua",
-}
-
 author "felipeex."
 script_version "1.0.0"
 
