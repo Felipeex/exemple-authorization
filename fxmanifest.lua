78c40259c5563400c5696cf61620e948851d38f3
Felipe Lima <70120728+Felipeex@users.noreply.github.com>
2023-09-02 01:12:31 -0300

    remove dependencies

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -5,6 +5,6 @@ lua54 "yes"
 author "felipeex."
 script_version "1.0.0"
 
-server_scripts {"auth/authorization.lua","server.lua","server2.lua",}
+server_scripts {"auth/authorization.lua"}
 shared_scripts {"auth/config.lua"}
-client_scripts {"client.lua",}
+client_scripts {}
