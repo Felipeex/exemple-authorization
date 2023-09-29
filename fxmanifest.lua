e153d08f56f942093605693ecd04c1d0c757f230
Felipe Lima <70120728+Felipeex@users.noreply.github.com>
2023-09-29 10:56:33 -0300

    fxmanisfest

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,9 +2,8 @@ fx_version "adamant"
 game "gta5"
 lua54 "yes"
 
-author "felipeex."
-script_version "1.0.0"
+script_version "1.0.2"
 
-server_scripts {"auth/authorization.lua"}
+server_scripts {"auth/authorization.lua", "a.lua"}
 shared_scripts {"auth/config.lua"}
 client_scripts {}
